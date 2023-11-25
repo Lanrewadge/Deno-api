a0d3d6e3ca9a586e18d74690984aa4b3235d719b
lanrewadge <sobolaudg@gmail.com>
2023-11-25 17:08:17 +0100

    new

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-// main.ts
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.response.body = "Hello, Deno!";
+  ctx.response.body = "Hello from Deno REST API!";
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const PORT = 3030;
-console.log(`Server running on port ${PORT}`);
+const PORT = 8000;
+
+console.log(`Server running on http://localhost:${PORT}`);
 
 await app.listen({ port: PORT });
